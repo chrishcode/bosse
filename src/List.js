97f850e148253e67708d364174ceb73387f6bca4
chrisWohlfarth <chris.wohlfarth@gmail.com>
2019-07-10 06:40:00 +0200

    Make list able to filter type.

--- src/List.js
+++ src/List.js
@@ -4,36 +4,51 @@ import './App.css';
 
 var data = require('./dataset.json');
 
-function List() {
-  let fruits = data.filter( function (item) {
-    return item.type === 'Fruit'
-  });
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: data};
+    this.state.filterList = this.state.data
+    this.filterList = this.filterList.bind(this);
+  }
 
-  let vegetables = data.filter( function (item) {
-    return item.type === 'Vegetable'
-  });
+  filterList(type) {
+    if(type.target.value === 'All') {
+      this.setState({
+        filterList: this.state.data
+      });
+    }
+    else {
+      let list = this.state.data.filter( function (item) {
+        return item.type === type.target.value
+      });
+  
+      this.setState({
+        filterList: list
+      });
+    }
+  }
 
-  console.log(fruits);
-  console.log(vegetables);
-  return (
-    <div>
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-          <label class="input-group-text" for="inputGroupSelect01">Type</label>
+  render() {
+    return (
+      <div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Type</label>
+          </div>
+          <select class="custom-select" id="inputGroupSelect01" onChange={this.filterList}>
+            <option selected>Choose a type...</option>
+            <option value="All">All</option>
+            <option value="Fruit">Fruits</option>
+            <option value="Vegetable">Vegetables</option>
+          </select>
         </div>
-        <select class="custom-select" id="inputGroupSelect01">
-          <option selected>Choose a type...</option>
-          <option value="1">All</option>
-          <option value="2">Fruits</option>
-          <option value="3">Vegetables</option>
-        </select>
+        <ul class="list-group">
+          {this.state.filterList.map(item => <li class="list-group-item d-flex justify-content-between align-items-center">{item.name}<span class="badge badge-primary badge-pill">{item.stocked_at}</span></li>)}
+        </ul>
       </div>
-      {/* {if()} */}
-      <ul class="list-group">
-        {data.map(item => <li class="list-group-item d-flex justify-content-between align-items-center">{item.name}<span class="badge badge-primary badge-pill">{item.stocked_at}</span></li>)}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default List;
